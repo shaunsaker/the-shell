e469dfa20d56221d6ebf6b945896682bbae9a37f
Shaun Saker <sakershaun@gmail.com>
2023-07-05 12:36:49 +1000

    fix: copy billing details to customer when there is no phone number attached

--- supabase/functions/_shared/supabase/copyBillingDetailsToCustomer.ts
+++ supabase/functions/_shared/supabase/copyBillingDetailsToCustomer.ts
@@ -8,8 +8,6 @@ export const copyBillingDetailsToCustomer = async (uuid: string, payment_method:
   const customer = payment_method.customer as string
   const { name, phone, address } = payment_method.billing_details
 
-  if (!name || !phone || !address) return
-
   // @ts-expect-error FIXME: stripe types in Deno are currently broken
   await stripe.customers.update(customer, { name, phone, address })
 
