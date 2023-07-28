6035c9e9192217c7a2d36f7730d1267b295d6228
Shaun Saker <sakershaun@gmail.com>
2023-07-28 16:08:26 +1000

    fix: emails:lint

--- packages/emails/types/environment.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {}
-  }
-}
-
-// If this file has no import/export statements (i.e. is a script)
-// convert it into a module by adding an empty export statement.
-export {}
