65d63cb9a4c8113b43abf1871fedd5e85c37ffd5
Shaun Saker <sakershaun@gmail.com>
2023-08-06 13:22:01 +1000

    feat: replace @tremor Heading component with a custom version

--- packages/app/src/components/heading/Heading.tsx
+++ packages/app/src/components/heading/Heading.tsx
@@ -1,8 +1,12 @@
-import { Title as TitlePrimitive, TitleProps } from '@tremor/react'
-import React, { ReactElement } from 'react'
+import React, { HTMLProps, ReactElement } from 'react'
+import { twMerge } from 'tailwind-merge'
 
-type Props = TitleProps
+type Props = HTMLProps<HTMLHeadingElement>
 
-export const Heading = ({ ...props }: Props): ReactElement => {
-  return <TitlePrimitive {...props} />
+export const Heading = ({ className = '', children, ...props }: Props): ReactElement => {
+  return (
+    <h2 className={twMerge('font-medium text-lg text-tremor-content-emphasis', className)} {...props}>
+      {children}
+    </h2>
+  )
 }
