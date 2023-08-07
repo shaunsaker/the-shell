00ef931b0a2d0674e9b105b97c772d0f62638b18
Shaun Saker <sakershaun@gmail.com>
2023-08-08 08:43:16 +1000

    feat: replace @tremor TextInput component with a custom version

--- packages/app/src/components/textInput/TextInput.tsx
+++ packages/app/src/components/textInput/TextInput.tsx
@@ -1,17 +1,19 @@
-import { TextInput as TextInputPrimitive } from '@tremor/react'
 import React, { ComponentProps, ReactElement } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 import { Text } from '../text/Text'
 
-type TextInputProps = { label?: string } & ComponentProps<typeof TextInputPrimitive>
+type TextInputProps = ComponentProps<'input'> & { label?: string }
 
 export const TextInput = ({ className = '', label, ...props }: TextInputProps): ReactElement => {
   return (
     <div className={twMerge('flex flex-col gap-y-2', className)}>
       {label && <Text>{label}</Text>}
 
-      <TextInputPrimitive {...props} />
+      <input
+        className="px-4 py-2 shadow-sm border rounded text-sm text-tremor-content dark:text-tremor-content hover:bg-tremor-background-muted dark:hover:bg-dark-tremor-background-muted focus-visible:outline-tremor-brand-subtle dark:focus-visible:outline-dark-tremor-brand-subtle transition-colors"
+        {...props}
+      />
     </div>
   )
 }
