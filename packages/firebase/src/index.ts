804676d614eb1df672dfa9cccaf13849e7bb67ed
Shaun Saker <sakershaun@gmail.com>
2023-08-30 15:01:55 +1000

    fix: firebase deploy

--- packages/firebase/src/index.ts
+++ packages/firebase/src/index.ts
@@ -1,24 +1,21 @@
-// import { createBillingPortalSessionFunction } from './functions/createBillingPortalSession'
-// import { createCheckoutSessionFunction } from './functions/createCheckoutSession'
-// import { deleteTeamFunction } from './functions/deleteTeam'
-// import { deleteUserAccountFunction } from './functions/deleteUserAccount'
-// import { inviteTeamMembersFunction } from './functions/inviteTeamMembers'
-// import { removeTeamMemberFunction } from './functions/removeTeamMember'
-// import { resendTeamInviteFunction } from './functions/resendTeamInvite'
-// import { stripeWebhookFunction } from './functions/stripeWebhook'
-// import { updateSubscriptionQuantityFunction } from './functions/updateSubscriptionQuantity'
-
-import { helloWorldFunction } from './functions/helloWorld'
+import { createBillingPortalSessionFunction } from './functions/createBillingPortalSession'
+import { createCheckoutSessionFunction } from './functions/createCheckoutSession'
+import { deleteTeamFunction } from './functions/deleteTeam'
+import { deleteUserAccountFunction } from './functions/deleteUserAccount'
+import { inviteTeamMembersFunction } from './functions/inviteTeamMembers'
+import { removeTeamMemberFunction } from './functions/removeTeamMember'
+import { resendTeamInviteFunction } from './functions/resendTeamInvite'
+import { stripeWebhookFunction } from './functions/stripeWebhook'
+import { updateSubscriptionQuantityFunction } from './functions/updateSubscriptionQuantity'
 
 export {
-  helloWorldFunction,
-  // createBillingPortalSessionFunction,
-  // createCheckoutSessionFunction,
-  // deleteTeamFunction,
-  // deleteUserAccountFunction,
-  // inviteTeamMembersFunction,
-  // removeTeamMemberFunction,
-  // resendTeamInviteFunction,
-  // stripeWebhookFunction,
-  // updateSubscriptionQuantityFunction,
+  createBillingPortalSessionFunction,
+  createCheckoutSessionFunction,
+  deleteTeamFunction,
+  deleteUserAccountFunction,
+  inviteTeamMembersFunction,
+  removeTeamMemberFunction,
+  resendTeamInviteFunction,
+  stripeWebhookFunction,
+  updateSubscriptionQuantityFunction,
 }
