ba014a38118c14fd95e3dd70a76dae033f938e1d
Shaun Saker <sakershaun@gmail.com>
2023-08-30 15:17:44 +1000

    fix(app): sign in with password

--- packages/app/src/auth/api/signInWithPassword.ts
+++ packages/app/src/auth/api/signInWithPassword.ts
@@ -1,7 +1,7 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { signInWithEmailAndPassword } from 'firebase/auth'
 
 import { auth } from '../../firebase'
 
 export const signInWithPassword = async ({ email, password }: { email: string; password: string }) => {
-  await createUserWithEmailAndPassword(auth, email, password)
+  await signInWithEmailAndPassword(auth, email, password)
 }
