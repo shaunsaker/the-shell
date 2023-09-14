7cc38efbb5a48eaabfa1a9ad16e1494860f3b380
Shaun Saker <sakershaun@gmail.com>
2023-09-14 10:16:58 +1000

    fix(app): when updating my user data, fetch the new user data

--- packages/app/src/users/hooks/useUpdateUser.ts
+++ packages/app/src/users/hooks/useUpdateUser.ts
@@ -1,12 +1,17 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { toast } from 'react-hot-toast'
 
+import { QueryKeys } from '../../models'
 import { updateUser } from '../api/updateUser'
 
 export const useUpdateUser = () => {
+  const queryClient = useQueryClient()
+
   return useMutation({
     mutationFn: updateUser,
     onSuccess: () => {
+      queryClient.invalidateQueries([QueryKeys.User])
+
       toast.success('Your details have been updated successfully.')
     },
   })
