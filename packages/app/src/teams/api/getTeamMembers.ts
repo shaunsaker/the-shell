088c38d20153b19dc09d9003cecf0b45f0e55e21
Shaun Saker <sakershaun@gmail.com>
2023-09-15 13:52:44 +1000

    feat(app): sort team members by createdAt

--- packages/app/src/teams/api/getTeamMembers.ts
+++ packages/app/src/teams/api/getTeamMembers.ts
@@ -1,10 +1,12 @@
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, orderBy, query } from 'firebase/firestore'
 import { TeamMember } from 'types'
 
 import { db } from '../../firebase'
 
 export const getTeamMembers = async (teamId: string) => {
-  const teamMembers = await getDocs(collection(db, 'teams', teamId, 'members'))
+  const ref = collection(db, 'teams', teamId, 'members')
+  const refQuery = query(ref, orderBy('createdAt', 'asc'))
+  const teamMembers = await getDocs(refQuery)
 
   return teamMembers.docs.map(doc => ({
     id: doc.id,
