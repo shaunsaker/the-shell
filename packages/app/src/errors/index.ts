222bc9447e3028281c9ebc74619d11b7f0ebbb74
Shaun Saker <sakershaun@gmail.com>
2023-09-18 15:14:59 +1000

    feat(app): ignore certain auth-related errors in Sentry

--- packages/app/src/errors/index.ts
+++ packages/app/src/errors/index.ts
@@ -2,11 +2,17 @@ import * as Sentry from '@sentry/react'
 
 const SENTRY_DSN = import.meta.env.VITE_SENTRY_DSN
 const ENVIRONMENT = import.meta.env.VITE_SENTRY_ENV
+const IGNORED_SENTRY_ERRORS: string[] = [
+  'auth/wrong-password',
+  'auth/network-request-failed',
+  'auth/email-already-in-use',
+]
 
 if (SENTRY_DSN && ENVIRONMENT) {
   Sentry.init({
     dsn: SENTRY_DSN,
     environment: ENVIRONMENT,
+    ignoreErrors: IGNORED_SENTRY_ERRORS,
   })
 }
 
