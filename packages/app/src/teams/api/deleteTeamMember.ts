56756d3385a75cef5df0b56d5424416cc807595c
Shaun Saker <sakershaun@gmail.com>
2023-09-19 08:21:40 +1000

    refactor(app): remove unused deleteTeamMember

--- packages/app/src/teams/api/deleteTeamMember.ts
+++
@@ -1,7 +0,0 @@
-import { deleteDoc, doc } from 'firebase/firestore'
-
-import { db } from '../../firebase'
-
-export const removeTeamMember = async ({ teamId, teamMemberId }: { teamId: string; teamMemberId: string }) => {
-  await deleteDoc(doc(db, 'teams', teamId, 'members', teamMemberId))
-}
