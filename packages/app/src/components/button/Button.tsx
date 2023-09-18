894b93427680a23ae7f861614ccecd84b5ca1790
Shaun Saker <sakershaun@gmail.com>
2023-09-18 12:57:51 +1000

    refactor(app): move Button Spinner to component

--- packages/app/src/components/button/Button.tsx
+++ packages/app/src/components/button/Button.tsx
@@ -1,6 +1,7 @@
-import { AnimatePresence, motion } from 'framer-motion'
 import React, { ComponentPropsWithoutRef, ReactElement, ReactNode } from 'react'
 import { twMerge } from 'tailwind-merge'
+
+import { Spinner } from './Spinner'
 
 type Variant = 'primary' | 'secondary' | 'light'
 type Color = string
@@ -72,30 +73,7 @@ export const Button = ({
       )}
       {...props}
     >
-      <AnimatePresence>
-        {loading && (
-          <motion.div
-            initial={{
-              opacity: 0,
-              width: '0px',
-            }}
-            animate={{ opacity: 1, width: 'auto' }}
-            exit={{ opacity: 0, width: '0px' }}
-          >
-            <svg className="h-4 w-4 animate-spin" viewBox="0 0 24 24">
-              <circle
-                className="fill-transparent opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"></path>
-            </svg>
-          </motion.div>
-        )}
-      </AnimatePresence>
+      <Spinner loading={loading} />
 
       {icon && iconPosition === 'left' && iconComponent}
 
---
+++ packages/app/src/components/button/Spinner.tsx
@@ -0,0 +1,35 @@
+import { AnimatePresence, motion } from 'framer-motion'
+import React, { ReactElement } from 'react'
+
+type SpinnerProps = {
+  loading?: boolean
+}
+
+export const Spinner = ({ loading }: SpinnerProps): ReactElement => {
+  return (
+    <AnimatePresence>
+      {loading && (
+        <motion.div
+          initial={{
+            opacity: 0,
+            width: '0px',
+          }}
+          animate={{ opacity: 1, width: 'auto' }}
+          exit={{ opacity: 0, width: '0px' }}
+        >
+          <svg className="h-4 w-4 animate-spin" viewBox="0 0 24 24">
+            <circle
+              className="fill-transparent opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"></path>
+          </svg>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  )
+}
