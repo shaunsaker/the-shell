b489f56ec803fd19453954c4847b32d6a1caecc7
Shaun Saker <sakershaun@gmail.com>
2023-09-23 14:07:07 +1000

    fix(app): move toasts to bottom

--- packages/app/src/components/snackbar/Snackbar.tsx
+++ packages/app/src/components/snackbar/Snackbar.tsx
@@ -13,20 +13,16 @@ const mapTypeToKind: Record<ToastType, AlertKind> = {
 }
 
 export const Snackbar = (): ReactElement => {
-  const { toasts, handlers } = useToaster()
+  const { toasts, handlers } = useToaster({ duration: 5000 })
   const { startPause, endPause } = handlers
 
   return (
-    <div
-      className="fixed inset-x-0 top-0 z-50 flex flex-col items-center"
-      onMouseEnter={startPause}
-      onMouseLeave={endPause}
-    >
+    <div className="fixed inset-x-0 bottom-0 z-50 flex flex-col items-center">
       <AnimatePresence initial={false}>
         {toasts
           .filter(toast => toast.visible)
           .map((toastInstance, index) => {
-            const initial = { opacity: 0, y: -64 * (index + 1), scale: 0.9 }
+            const initial = { opacity: 0, y: 64 * (index + 1), scale: 0.9 }
 
             return (
               <motion.div
@@ -38,9 +34,11 @@ export const Snackbar = (): ReactElement => {
                   scale: 1,
                 }}
                 exit={initial}
+                onMouseEnter={startPause}
+                onMouseLeave={endPause}
               >
                 <Alert
-                  className="mt-2 shadow-lg"
+                  className="mb-4 shadow-lg"
                   kind={mapTypeToKind[toastInstance.type]}
                   onClose={() => {
                     toast.dismiss(toastInstance.id)
