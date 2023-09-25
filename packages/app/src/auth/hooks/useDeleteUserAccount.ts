8020dbeee1bb7ceda94f159cbd26a81af19f3afa
Shaun Saker <sakershaun@gmail.com>
2023-09-25 17:17:20 +1000

    refactor(app): useDeleteUserAccount move signOut to api portion

--- packages/app/src/auth/hooks/useDeleteUserAccount.ts
+++ packages/app/src/auth/hooks/useDeleteUserAccount.ts
@@ -2,16 +2,11 @@ import { useMutation } from '@tanstack/react-query'
 import { toast } from 'react-hot-toast'
 
 import { deleteUserAccount } from '../api/deleteUserAccount'
-import { useSignOut } from './useSignOut'
 
 export const useDeleteUserAccount = () => {
-  const { mutate: signOut } = useSignOut()
-
   return useMutation({
     mutationFn: deleteUserAccount,
     onSuccess: async () => {
-      await signOut()
-
       toast.success('Your account has been deleted successfully.')
     },
   })
