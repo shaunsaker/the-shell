93460a0192bf836b8e54f1002e2285e9846ce541
Shaun Saker <sakershaun@gmail.com>
2023-09-28 15:15:42 +1000

    test(app): getSubscriptionSeatsButtonLabel tests

---
+++ packages/app/src/utils/getSubscriptionSeatsButtonLabel.ts
@@ -0,0 +1,19 @@
+import { Price } from 'types'
+
+import { formatCurrency } from './formatCurrency'
+import { maybePluralise } from './maybePluralise'
+
+export const getSubscriptionSeatsButtonLabel = (numberOfNewSeats: number, activePrice: Price): string => {
+  if (numberOfNewSeats === 0) {
+    return 'Update plan'
+  }
+
+  const seatsText = `${Math.abs(numberOfNewSeats)} ${maybePluralise(Math.abs(numberOfNewSeats), 'seat')}`
+  const costText = formatCurrency((Math.abs(numberOfNewSeats) * activePrice.unitAmount) / 100, activePrice.currency)
+
+  if (numberOfNewSeats > 0) {
+    return `Add ${seatsText} for an additional ${costText} per ${activePrice.interval}`
+  }
+
+  return `Remove ${seatsText} for ${costText} per ${activePrice.interval} less`
+}
