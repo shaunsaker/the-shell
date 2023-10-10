e351647cc90f552ca970e34b8a878c40a0e425c7
Shaun Saker <sakershaun@gmail.com>
2023-10-10 14:30:52 +1000

    feat(components): add ErrorPage component

---
+++ packages/components/src/components/errorPage/ErrorPage.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { ErrorPage } from './ErrorPage'
+
+const meta = {
+  title: 'ErrorPage',
+  component: ErrorPage,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof ErrorPage>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    title: 'No data',
+    description: 'There is no data to display.',
+    onGoHomeClick: () => {
+      console.log('Go home clicked')
+    },
+    onContactSupportClick: () => {
+      console.log('Contact support clicked')
+    },
+  },
+}
