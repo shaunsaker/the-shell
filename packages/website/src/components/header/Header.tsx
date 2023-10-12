0031b587262f2e673ad0ed6e8be0320b4e24ef84
Shaun Saker <sakershaun@gmail.com>
2023-10-13 08:53:21 +1000

    feat(website): mobile navigation

--- packages/website/src/components/header/Header.tsx
+++ packages/website/src/components/header/Header.tsx
@@ -1,66 +1,109 @@
 'use client'
 
-import { Button, Headerbar, Logo } from 'components'
+import { Bars3Icon } from '@heroicons/react/24/outline'
+import { Button, Headerbar, Logo, Popover, Sidebar } from 'components'
 import { useRouter } from 'next/navigation'
-import React, { ReactElement } from 'react'
+import React, { ComponentProps, ReactElement, useState } from 'react'
 
 import { routes } from '@/routes'
 
-const NAV_LINKS = [
+type NavigationItem = ComponentProps<typeof Sidebar>['items'][0]
+
+const NAVIGATION_ITEMS: NavigationItem[] = [
   {
-    label: 'Features',
+    name: 'Features',
     href: routes.features,
   },
   {
-    label: 'Testimonials',
+    name: 'Testimonials',
     href: routes.testimonials,
   },
   {
-    label: 'Pricing',
+    name: 'Pricing',
     href: routes.pricing,
   },
 ]
 
-// TODO: SS mobile menu
+const SIGN_IN_NAVIGATION_ITEM: NavigationItem = {
+  name: 'Sign in',
+  href: process.env.NEXT_PUBLIC_APP_SIGN_IN_URL,
+}
+
 export const Header = (): ReactElement => {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
   const router = useRouter()
 
   return (
-    <Headerbar>
-      <button className="mr-4 flex items-center">
-        <Logo />
-      </button>
-
-      {NAV_LINKS.map((link) => (
+    <>
+      <Headerbar>
         <Button
-          key={link.href}
+          className="-ml-3 lg:hidden"
           variant="lightNeutral"
           onClick={() => {
-            router.push(link.href)
+            setSidebarOpen(true)
           }}
         >
-          {link.label}
-        </Button>
-      ))}
-
-      <div className="flex flex-1 items-center justify-end gap-x-2">
-        <Button
-          variant="lightNeutral"
-          onClick={() => {
-            router.push(process.env.NEXT_PUBLIC_APP_SIGN_IN_URL)
-          }}
-        >
-          Sign in
+          <Bars3Icon className="h-6 w-6" />
         </Button>
 
-        <Button
-          onClick={() => {
-            router.push(process.env.NEXT_PUBLIC_APP_SIGN_UP_URL)
+        <button className="mr-4 flex items-center">
+          <Logo />
+        </button>
+
+        <div className="hidden items-center gap-x-2 lg:flex">
+          {NAVIGATION_ITEMS.map((link) => (
+            <Button
+              key={link.href}
+              variant="lightNeutral"
+              onClick={() => {
+                router.push(link.href)
+              }}
+            >
+              {link.name}
+            </Button>
+          ))}
+        </div>
+
+        <div className="flex flex-1 items-center justify-end gap-x-2">
+          <div className="hidden lg:block">
+            <Button
+              variant="lightNeutral"
+              onClick={() => {
+                router.push(SIGN_IN_NAVIGATION_ITEM.href)
+              }}
+            >
+              {SIGN_IN_NAVIGATION_ITEM.name}
+            </Button>
+          </div>
+
+          <Button
+            onClick={() => {
+              router.push(process.env.NEXT_PUBLIC_APP_SIGN_UP_URL)
+            }}
+          >
+            Get started today
+          </Button>
+        </div>
+      </Headerbar>
+
+      <div className="lg:hidden">
+        <Popover
+          open={sidebarOpen}
+          onClose={() => {
+            setSidebarOpen(false)
           }}
         >
-          Get started today
-        </Button>
+          <Sidebar
+            items={[...NAVIGATION_ITEMS, SIGN_IN_NAVIGATION_ITEM]}
+            onItemClick={(href) => {
+              router.push(href)
+
+              setSidebarOpen(false)
+            }}
+          />
+        </Popover>
       </div>
-    </Headerbar>
+    </>
   )
 }
