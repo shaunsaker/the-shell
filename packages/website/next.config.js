75098b06fe27a55559efb6b8a41fa01ebf883947
Shaun Saker <sakershaun@gmail.com>
2023-10-12 13:50:08 +1000

    build(website): enable svg support

--- packages/website/next.config.js
+++ packages/website/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  transpilePackages: ['components'],
+
+  // adds support for importing svg's as components
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
+}
 
 module.exports = nextConfig
