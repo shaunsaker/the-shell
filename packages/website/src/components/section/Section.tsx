113b1abe9eec77a13b9d5352949b4cbe71e93466
Shaun Saker <sakershaun@gmail.com>
2023-10-15 16:30:14 +1000

    feat(website): PrimaryFeatures section

--- packages/website/src/components/section/Section.tsx
+++ packages/website/src/components/section/Section.tsx
@@ -9,7 +9,10 @@ export const Section = ({
   ...props
 }: Props): ReactElement => {
   return (
-    <section className={twMerge('px-8 py-24 lg:px-16', className)} {...props}>
+    <section
+      className={twMerge('px-8 py-12 lg:px-16 lg:py-24', className)}
+      {...props}
+    >
       {children}
     </section>
   )
