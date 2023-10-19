89ef098a729502f52eca322be4eec6f834f81ae7
Shaun Saker <sakershaun@gmail.com>
2023-10-19 15:30:48 +1000

    fix(functions): handle all errors

--- packages/functions/src/functions/onTeamMemberDeleted/index.ts
+++ packages/functions/src/functions/onTeamMemberDeleted/index.ts
@@ -7,31 +7,37 @@ import { getSubscriptionSeats } from '@/billing/getSubscriptionSeats'
 import { getTeamMembersForUserEmail } from '@/teams/getTeamMembersForUserEmail'
 
 export const onTeamMemberDeleted = onDocumentDeleted('teams/{teamsId}/members/{memberId}', async event => {
-  const teamMember = event.data?.data() as TeamMember | undefined
+  try {
+    const teamMember = event.data?.data() as TeamMember | undefined
 
-  if (!teamMember) {
-    throw new HttpsError('not-found', 'Team member not found')
+    if (!teamMember) {
+      throw new HttpsError('not-found', 'Team member not found')
+    }
+
+    const usersTeamMembers = await getTeamMembersForUserEmail(teamMember.email)
+
+    // this is the last team the user was removed from when there are no more other team members with the same email
+    const isLastTeam = !usersTeamMembers.length
+
+    // we should only delete a user's subscription seat if this is the last team the user is a member of
+    if (!isLastTeam) {
+      return
+    }
+
+    // get the subscription seats for the admin that added this team member
+    const invitersSubscriptionSeats = await getSubscriptionSeats(teamMember.invitedBy)
+
+    // NOTE: this assumes that the user only has one subscription
+    const subscriptionId = invitersSubscriptionSeats[0].subscriptionId
+
+    // if a user has not signed up yet, userId will be null so we need to use email
+    await deleteAssignedSubscriptionSeatsByEmail({
+      subscriptionId,
+      email: teamMember.email,
+    })
+  } catch (error) {
+    console.error(error)
+
+    throw error
   }
-
-  const usersTeamMembers = await getTeamMembersForUserEmail(teamMember.email)
-
-  // this is the last team the user was removed from when there are no more other team members with the same email
-  const isLastTeam = !usersTeamMembers.length
-
-  // we should only delete a user's subscription seat if this is the last team the user is a member of
-  if (!isLastTeam) {
-    return
-  }
-
-  // get the subscription seats for the admin that added this team member
-  const invitersSubscriptionSeats = await getSubscriptionSeats(teamMember.invitedBy)
-
-  // NOTE: this assumes that the user only has one subscription
-  const subscriptionId = invitersSubscriptionSeats[0].subscriptionId
-
-  // if a user has not signed up yet, userId will be null so we need to use email
-  await deleteAssignedSubscriptionSeatsByEmail({
-    subscriptionId,
-    email: teamMember.email,
-  })
 })
--- packages/functions/src/functions/onUserCreated/index.ts
+++ packages/functions/src/functions/onUserCreated/index.ts
@@ -7,19 +7,25 @@ import { claimGuestSubscriptions } from './claimGuestSubscriptions'
 console.log('Hello from On User Created!')
 
 export const onUserCreated = onDocumentCreated('users/{userId}', async event => {
-  // if a new user was created, update all team members with the new user data
-  const { userId } = event.params
-  const data = event.data?.data() as User | undefined
+  try {
+    // if a new user was created, update all team members with the new user data
+    const { userId } = event.params
+    const data = event.data?.data() as User | undefined
 
-  if (!data) {
-    throw new Error('User not found')
+    if (!data) {
+      throw new Error('User not found')
+    }
+
+    if (!data.email) {
+      throw new Error('User email not found')
+    }
+
+    await claimGuestSubscriptions({ uid: userId, email: data.email })
+
+    await claimAssignedSeats({ uid: userId, email: data.email, firstName: data.firstName, lastName: data.lastName })
+  } catch (error) {
+    console.error(error)
+
+    throw error
   }
-
-  if (!data.email) {
-    throw new Error('User email not found')
-  }
-
-  await claimGuestSubscriptions({ uid: userId, email: data.email })
-
-  await claimAssignedSeats({ uid: userId, email: data.email, firstName: data.firstName, lastName: data.lastName })
 })
