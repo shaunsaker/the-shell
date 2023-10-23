ae40ca2ae94d1d711ba09a2ad76a51629dbc0001
Shaun Saker <sakershaun@gmail.com>
2023-10-23 21:04:39 +1000

    fix(website): squashed Error page

--- packages/components/src/components/slimLayout/SlimLayout.tsx
+++ packages/components/src/components/slimLayout/SlimLayout.tsx
@@ -7,7 +7,7 @@ type Props = ComponentPropsWithoutRef<'div'>
 
 export const SlimLayout = ({ className, children, ...props }: Props) => {
   return (
-    <div className={twMerge('relative flex min-h-full justify-center md:px-12 lg:px-0', className)} {...props}>
+    <div className={twMerge('relative flex min-h-screen justify-center md:px-12 lg:px-0', className)} {...props}>
       <div className="bg-theme-background dark:bg-dark-theme-background relative z-10 flex flex-1 flex-col justify-center px-4 py-10 md:flex-none md:px-28">
         <div className="mx-auto w-full max-w-md sm:px-4 md:w-96 md:max-w-sm md:px-0">{children}</div>
       </div>
