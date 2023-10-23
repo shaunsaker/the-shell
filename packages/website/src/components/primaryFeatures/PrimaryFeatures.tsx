be7e43807746634de1893ff645f1e0d9950483d4
Shaun Saker <sakershaun@gmail.com>
2023-10-23 21:21:05 +1000

    feat(website): move images to public folder

--- packages/website/src/components/primaryFeatures/PrimaryFeatures.tsx
+++ packages/website/src/components/primaryFeatures/PrimaryFeatures.tsx
@@ -3,28 +3,9 @@
 import { app } from 'config'
 import React from 'react'
 
-import screenshotExpenses from '@/assets/screenshots/expenses.png'
-import screenshotPayroll from '@/assets/screenshots/payroll.png'
-import screenshotReporting from '@/assets/screenshots/reporting.png'
-import screenshotVatReturns from '@/assets/screenshots/vat-returns.png'
 import { routes } from '@/routes'
 
 import { Features } from '../features/Features'
-
-const getFeatureScreenshot = (feature: string) => {
-  switch (feature) {
-    case 'Feature 1':
-      return screenshotPayroll
-    case 'Feature 2':
-      return screenshotExpenses
-    case 'Feature 3':
-      return screenshotVatReturns
-    case 'Feature 4':
-      return screenshotReporting
-    default:
-      return screenshotExpenses
-  }
-}
 
 export const PrimaryFeatures = () => {
   return (
@@ -35,7 +16,7 @@ export const PrimaryFeatures = () => {
       features={app.website.primaryFeatures.features.map(feature => ({
         title: feature.title,
         description: feature.description,
-        image: getFeatureScreenshot(feature.title),
+        image: feature.image,
       }))}
     />
   )
--- packages/website/src/components/secondaryFeatures/SecondaryFeatures.tsx
+++ packages/website/src/components/secondaryFeatures/SecondaryFeatures.tsx
@@ -3,27 +3,7 @@
 import { app } from 'config'
 import React from 'react'
 
-import screenshotExpenses from '@/assets/screenshots/expenses.png'
-import screenshotPayroll from '@/assets/screenshots/payroll.png'
-import screenshotReporting from '@/assets/screenshots/reporting.png'
-import screenshotVatReturns from '@/assets/screenshots/vat-returns.png'
-
 import { Features } from '../features/Features'
-
-const getFeatureScreenshot = (feature: string) => {
-  switch (feature) {
-    case 'Feature 1':
-      return screenshotPayroll
-    case 'Feature 2':
-      return screenshotExpenses
-    case 'Feature 3':
-      return screenshotVatReturns
-    case 'Feature 4':
-      return screenshotReporting
-    default:
-      return screenshotExpenses
-  }
-}
 
 export const SecondaryFeatures = () => {
   return (
@@ -34,7 +14,7 @@ export const SecondaryFeatures = () => {
       features={app.website.secondaryFeatures.features.map(feature => ({
         title: feature.title,
         description: feature.description,
-        image: getFeatureScreenshot(feature.title),
+        image: feature.image,
       }))}
     />
   )
