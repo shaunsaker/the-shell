e134479316d941431e19762e8dc85538e037aade
Shaun Saker <sakershaun@gmail.com>
2023-10-26 14:17:01 +1000

    fix(app): change email in local development

--- packages/app/src/auth/api/sendChangeEmailVerification.ts
+++ packages/app/src/auth/api/sendChangeEmailVerification.ts
@@ -8,11 +8,11 @@ import { signOut } from './signOut'
 const sendChangeEmailVerificationFunction = invokeFunction(Functions.sendChangeEmailVerification)
 
 export const sendChangeEmailVerification = async ({ oldEmail, newEmail }: { oldEmail: string; newEmail: string }) => {
-  await signOut()
-
   await sendChangeEmailVerificationFunction({
     oldEmail,
     newEmail,
     redirectUrl: `${window.location.origin}${routes.userManagement}`,
   })
+
+  await signOut()
 }
