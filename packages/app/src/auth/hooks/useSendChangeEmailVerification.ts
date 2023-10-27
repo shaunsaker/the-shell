a8c656ece8bd604fc2be8c97f672a0b90534c27d
Shaun Saker <sakershaun@gmail.com>
2023-10-27 19:00:17 +1000

    fix(app): change email not reflecting latest user data

--- packages/app/src/auth/hooks/useSendChangeEmailVerification.ts
+++ packages/app/src/auth/hooks/useSendChangeEmailVerification.ts
@@ -4,15 +4,20 @@ import toast from 'react-hot-toast'
 import { useUserEmail } from '@/user/hooks/useUserEmail'
 
 import { sendChangeEmailVerification } from '../api/sendChangeEmailVerification'
+import { useSignOut } from './useSignOut'
 
-const IS_DEVELOPMENT = import.meta.env.DEV
+const IS_DEVELOPMENT = import.meta.env.MODE === 'development'
 
 export const useSendChangeEmailVerification = () => {
   const [_, setUserEmail] = useUserEmail()
 
+  const { mutate: signOut } = useSignOut()
+
   return useMutation({
     mutationFn: sendChangeEmailVerification,
     onSuccess: async (_, { newEmail }) => {
+      signOut()
+
       // update the user's email in the app for quicker sign in
       setUserEmail(newEmail)
 
--- packages/app/src/billing/hooks/useSubscriptionInfoListener.ts
+++ packages/app/src/billing/hooks/useSubscriptionInfoListener.ts
@@ -1,20 +1,22 @@
 import { useQueryClient } from '@tanstack/react-query'
 import { useEffect } from 'react'
 
+import { useAuthUser } from '@/auth/hooks/useAuthUser'
 import { QueryKeys } from '@/types'
 
 import { listenSubscriptionInfo } from '../api/listenSubscriptionInfo'
 import { useSubscriptionSeats } from './useSubscriptionSeats'
 
 export const useSubscriptionInfoListener = () => {
-  const { data: subscriptionSeats } = useSubscriptionSeats()
   const queryClient = useQueryClient()
+  const { data: subscriptionSeats } = useSubscriptionSeats()
+  const { data: authUser } = useAuthUser()
 
   // NOTE: this assumes we only have one subscription per user
   const subscriptionId = subscriptionSeats?.length && subscriptionSeats[0].subscriptionId
 
   useEffect(() => {
-    if (subscriptionId) {
+    if (authUser && subscriptionId) {
       // subscribe to the subscription info changes
       const unsubscribe = listenSubscriptionInfo(subscriptionId, subscriptionInfo => {
         queryClient.setQueryData([QueryKeys.SubscriptionInfo], subscriptionInfo)
@@ -22,5 +24,5 @@ export const useSubscriptionInfoListener = () => {
 
       return unsubscribe
     }
-  }, [queryClient, subscriptionId])
+  }, [authUser, queryClient, subscriptionId])
 }
