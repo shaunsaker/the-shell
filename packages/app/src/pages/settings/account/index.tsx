834618c9b67bf869c99ce56a39ee997a8f833f37
Shaun Saker <sakershaun@gmail.com>
2023-10-31 14:48:56 +1000

    feat(app): remove change email feature

--- packages/app/src/pages/settings/account/index.tsx
+++ packages/app/src/pages/settings/account/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import { ChangeEmailSection } from './components/changeEmailSection/ChangeEmailSection'
 import { ChangeNameSection } from './components/changeNameSection/ChangeNameSection'
 import { ChangePasswordSection } from './components/changePasswordSection/ChangePasswordSection'
 import { DeleteAccountSection } from './components/deleteAccountSection/DeleteAccountSection'
@@ -9,8 +8,6 @@ export const SettingsAccount = () => {
   return (
     <div>
       <ChangeNameSection />
-
-      <ChangeEmailSection />
 
       <ChangePasswordSection />
 
