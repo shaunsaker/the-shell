4e8f7f3080366af6b52ef4e56db11bd9be04872b
Shaun Saker <sakershaun@gmail.com>
2023-11-03 07:01:24 +1000

    fix(website): Sections should clear Header when scrolled

--- packages/website/src/components/section/Section.tsx
+++ packages/website/src/components/section/Section.tsx
@@ -5,7 +5,9 @@ type Props = ComponentPropsWithoutRef<'section'>
 
 export const Section = ({ className = '', children, ...props }: Props) => {
   return (
-    <section className={twMerge('px-4 sm:px-8 py-12 lg:px-16 lg:py-24', className)} {...props}>
+    // 63px is the height of the Header
+    // 95px is the height of the Header + pt-12
+    <section className={twMerge('px-4 sm:px-8 py-12 lg:px-16 lg:py-24 pt-[95px] lg:pt-[63px]', className)} {...props}>
       {children}
     </section>
   )
