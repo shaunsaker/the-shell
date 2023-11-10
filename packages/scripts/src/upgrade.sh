f672d43c1515bbd140cc92bdf89291f853f3c7f4
Shaun Saker <sakershaun@gmail.com>
2023-11-11 08:25:35 +1000

    feat(scripts): add upgrade script

---
+++ packages/scripts/src/upgrade.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Check if a remote branch was passed as an argument
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 'remote/branch'"
+  exit 1
+fi
+
+# The argument is the remote branch
+REMOTE_BRANCH="$1"
+
+# Fetch the latest changes from the remote
+git fetch $(echo $REMOTE_BRANCH | cut -d '/' -f 1)
+
+# Get commit messages from the current branch
+CURRENT_BRANCH_COMMITS=$(git log --format="%s")
+
+# Initialize an array to hold the commit hashes for cherry-picking
+declare -a commits_to_pick
+
+# Compare and gather commit hashes from the remote branch
+# that are not in the current branch and do not contain "custom" in their message
+while IFS= read -r line; do
+  commit_hash=$(echo $line | awk '{print $1}')
+  commit_message=$(echo $line | cut -d ' ' -f 2-)
+
+  if echo "$CURRENT_BRANCH_COMMITS" | grep -Fxq "$commit_message"; then
+    continue
+  elif [[ "$commit_message" != *"custom"* ]]; then
+    commits_to_pick+=("$commit_hash - $commit_message")
+  fi
+done < <(git log "$REMOTE_BRANCH" --format="%H %s")
+
+# Log the commit hashes and messages to be cherry-picked
+echo "Commits to be cherry-picked from $REMOTE_BRANCH:"
+for commit in "${commits_to_pick[@]}"; do
+  echo "$commit"
+done
+
+# Ask for user confirmation before cherry-picking
+read -p "Do you want to proceed with cherry-picking these commits? (yes/no) " confirmation
+if [[ "$confirmation" != "yes" ]]; then
+  echo "Cherry-picking aborted."
+  exit 0
+fi
+
+# Cherry-pick commits one by one
+for ((idx = ${#commits_to_pick[@]} - 1; idx >= 0; idx--)); do
+  commit_hash="$(echo "${commits_to_pick[idx]}" | cut -d ' ' -f 1)"
+  echo "Cherry-picking $commit_hash..."
+  git cherry-pick $commit_hash
+
+  # Check if cherry-pick is successful
+  if [ $? -ne 0 ]; then
+    echo "Conflict detected! Resolve the conflict and continue cherry-picking."
+    read -p "After resolving conflict, press enter to continue with the next commit..."
+    git cherry-pick --continue
+  fi
+done
+
+echo "Cherry-picking completed."
