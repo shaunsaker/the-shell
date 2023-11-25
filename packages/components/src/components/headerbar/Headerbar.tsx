6940e83660dddeb54e5844e0ce280e27b3fd63b6
Shaun Saker <sakershaun@gmail.com>
2023-11-25 11:13:49 +1000

    feat(components): remove brand bg in Headerbar on mobile

--- packages/components/src/components/headerbar/Headerbar.tsx
+++ packages/components/src/components/headerbar/Headerbar.tsx
@@ -7,7 +7,7 @@ export const Headerbar = ({ className = '', children, ...props }: Props) => {
   return (
     <header
       className={twMerge(
-        'bg-theme-brand lg:bg-theme-background dark:bg-dark-theme-brand lg:dark:bg-dark-theme-background border-theme-border dark:border-dark-theme-border flex h-16 w-full shrink-0 gap-x-1 border-b px-2 shadow-lg lg:gap-x-2 lg:px-8',
+        'bg-theme-background dark:bg-dark-theme-background border-theme-border dark:border-dark-theme-border flex h-16 w-full shrink-0 gap-x-1 border-b px-2 shadow-lg lg:gap-x-2 lg:px-8',
         className,
       )}
       {...props}
