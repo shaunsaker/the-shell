f7edeb008b9bfc56957f8371d4b09ce39798c875
Shaun Saker <sakershaun@gmail.com>
2023-11-25 11:12:18 +1000

    feat(utils): add validateUrl

---
+++ packages/utils/src/validateUrl/validateUrl.ts
@@ -0,0 +1,6 @@
+export const validateUrl = (url: string): boolean => {
+  const urlRegex =
+    /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
+
+  return urlRegex.test(url)
+}
