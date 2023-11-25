1026b8dabc31d273cd039d1d0ccdeb5cd4b4db32
Shaun Saker <sakershaun@gmail.com>
2023-11-25 11:16:26 +1000

    fix(colors): fix custom hover colors in dark mode

--- packages/app/tailwind.config.ts
+++ packages/app/tailwind.config.ts
@@ -17,42 +17,42 @@ module.exports = {
     {
       pattern:
         /^(bg-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-      variants: ['hover', 'ui-selected', 'dark'],
+      variants: ['hover', 'ui-selected', 'dark', 'dark:hover'],
     },
     {
       pattern:
         /^(text-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-      variants: ['hover', 'ui-selected', 'dark'],
+      variants: ['hover', 'ui-selected', 'dark', 'dark:hover'],
     },
     {
       pattern:
         /^(border-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-      variants: ['hover', 'ui-selected', 'dark'],
+      variants: ['hover', 'ui-selected', 'dark', 'dark:hover'],
     },
     {
       pattern:
         /^(ring-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-      variants: ['hover', 'ui-selected', 'dark'],
+      variants: ['hover', 'ui-selected', 'dark', 'dark:hover'],
     },
     {
       pattern:
         /^(stroke-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-      variants: ['hover', 'ui-selected', 'dark'],
+      variants: ['hover', 'ui-selected', 'dark', 'dark:hover'],
     },
     {
       pattern:
         /^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-      variants: ['hover', 'ui-selected', 'dark'],
+      variants: ['hover', 'ui-selected', 'dark', 'dark:hover'],
     },
     {
       pattern:
         /^(from-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-      variants: ['hover', 'ui-selected', 'dark'],
+      variants: ['hover', 'ui-selected', 'dark', 'dark:hover'],
     },
     {
       pattern:
         /^(to-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-      variants: ['hover', 'ui-selected', 'dark'],
+      variants: ['hover', 'ui-selected', 'dark', 'dark:hover'],
     },
   ],
 }
