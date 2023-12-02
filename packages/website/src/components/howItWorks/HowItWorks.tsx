83d7e82063dba90e42a61406c24216adc1df7f28
Shaun Saker <sakershaun@gmail.com>
2023-12-03 07:05:07 +1000

    fix(website): alignment of HowItWorks section on mobile and dark mode colours

--- packages/website/src/components/howItWorks/HowItWorks.tsx
+++ packages/website/src/components/howItWorks/HowItWorks.tsx
@@ -1,6 +1,7 @@
 import { ArrowRightIcon } from '@heroicons/react/24/solid'
 import { ParagraphText, TitleText } from 'components'
 import React, { Fragment } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 import { Container } from '../container/Container'
 import { Section, SectionProps } from '../section/Section'
@@ -12,30 +13,44 @@ type Props = {
   }[]
 } & SectionProps
 
-export const HowItWorks = ({ steps, ...sectionProps }: Props) => {
+export const HowItWorks = ({ variant, steps, ...sectionProps }: Props) => {
   return (
-    <Section {...sectionProps}>
+    <Section variant={variant} {...sectionProps}>
       <Container className="mt-12 lg:mt-24">
-        <ul className="gap-6 flex items-center flex-wrap sm:flex-nowrap">
-          {steps?.length
-            ? steps.map((step, index) => (
-                <Fragment key={step.title}>
-                  <li className="flex-1 flex flex-col items-center gap-2 text-center relative pt-16">
-                    <div className="opacity-30 absolute inset-0 text-9xl font-bold">{index + 1}</div>
+        <ul className="gap-6 flex items-center flex-col lg:flex-row">
+          {steps?.map((step, index) => (
+            <Fragment key={step.title}>
+              <li className="flex-1 flex flex-col items-center gap-2 text-center relative pt-16">
+                <div
+                  className={twMerge(
+                    'opacity-30 absolute inset-0 text-9xl font-bold',
+                    variant === 'default' ? 'text-theme-content-inverted' : '',
+                  )}
+                >
+                  {index + 1}
+                </div>
 
-                    <TitleText>{step.title}</TitleText>
+                <TitleText className={variant === 'default' ? 'text-theme-content-inverted' : ''}>
+                  {step.title}
+                </TitleText>
 
-                    <ParagraphText>{step.description}</ParagraphText>
-                  </li>
+                <ParagraphText className={variant === 'default' ? 'text-theme-content-inverted' : ''}>
+                  {step.description}
+                </ParagraphText>
+              </li>
 
-                  {index !== steps.length - 1 ? (
-                    <div>
-                      <ArrowRightIcon className="w-10 h-10" />
-                    </div>
-                  ) : null}
-                </Fragment>
-              ))
-            : null}
+              {index !== steps?.length - 1 ? (
+                <div>
+                  <ArrowRightIcon
+                    className={twMerge(
+                      'w-10 h-10 rotate-90 lg:rotate-0',
+                      variant === 'default' ? 'text-theme-content-inverted' : '',
+                    )}
+                  />
+                </div>
+              ) : null}
+            </Fragment>
+          ))}
         </ul>
       </Container>
     </Section>
