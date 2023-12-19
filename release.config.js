18227561b4a435a49310de12c35a400ca8e65727
Shaun Saker <sakershaun@gmail.com>
2023-12-20 09:24:59 +1000

    build: generate CHANGELOG and update package.json version

--- release.config.js
+++ release.config.js
@@ -6,6 +6,13 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
     '@semantic-release/github',
   ],
 }
