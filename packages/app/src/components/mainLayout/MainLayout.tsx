6f992e68919f96e5e8e0620d8a0d1b2965bef4f4
Shaun Saker <sakershaun@gmail.com>
2023-12-20 10:30:49 +1000

    refactor(app): move MainLayout Sidebar to AppSidebar

--- packages/app/src/components/mainLayout/MainLayout.tsx
+++ packages/app/src/components/mainLayout/MainLayout.tsx
@@ -1,71 +1,15 @@
-import { Cog6ToothIcon, EnvelopeOpenIcon, HomeModernIcon } from '@heroicons/react/24/outline'
-import { Popover, Sidebar } from 'components'
+import { Popover } from 'components'
 import React from 'react'
-import { Outlet, useLocation, useNavigate } from 'react-router-dom'
-import { useLink } from 'utils'
+import { Outlet } from 'react-router-dom'
 
-import { useHasActiveSubscription } from '@/billing/hooks/useHasActiveSubscription'
-import { features } from '@/features'
-import { routes } from '@/router/routes'
 import { useSidebarOpen } from '@/sidebar/hooks/useSidebarOpen'
 
-import { app } from '../../../../config'
+import { AppSidebar } from '../appSidebar/AppSidebar'
 
 export const MainLayout = () => {
-  const location = useLocation()
-  const { data: hasActiveSubscription, isLoading: hasActiveSubscriptionLoading } = useHasActiveSubscription()
   const [sidebarOpen, setSidebarOpen] = useSidebarOpen()
 
-  const navigate = useNavigate()
-  const link = useLink()
-
-  const sidebar = (
-    <Sidebar>
-      <Sidebar.Item
-        icon={<HomeModernIcon />}
-        active={location.pathname === routes.dashboard}
-        disabled={features.subscriptions && (hasActiveSubscriptionLoading || !hasActiveSubscription)}
-        onClick={() => {
-          navigate(routes.dashboard)
-
-          if (sidebarOpen) {
-            setSidebarOpen(false)
-          }
-        }}
-      >
-        Dashboard
-      </Sidebar.Item>
-
-      {(features.auth || features.subscriptions) && (
-        <Sidebar.Item
-          icon={<Cog6ToothIcon />}
-          active={location.pathname.startsWith(routes.settings)}
-          onClick={() => {
-            navigate(routes.settings)
-
-            if (sidebarOpen) {
-              setSidebarOpen(false)
-            }
-          }}
-        >
-          Settings
-        </Sidebar.Item>
-      )}
-
-      <Sidebar.Item
-        icon={<EnvelopeOpenIcon />}
-        onClick={() => {
-          link(`mailto:${app.emails.support}`, '_blank')
-
-          if (sidebarOpen) {
-            setSidebarOpen(false)
-          }
-        }}
-      >
-        Send Feedback
-      </Sidebar.Item>
-    </Sidebar>
-  )
+  const sidebar = <AppSidebar />
 
   return (
     <div className="flex h-full">
