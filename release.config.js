8116a829958c4f648d3c4e3fb7287e48eb7d8007
Shaun Saker <sakershaun@gmail.com>
2023-12-20 10:16:51 +1000

    build: commit release assets to git

--- release.config.js
+++ release.config.js
@@ -13,6 +13,13 @@ module.exports = {
         npmPublish: false,
       },
     ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
     '@semantic-release/github',
   ],
 }
