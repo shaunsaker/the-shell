8c998dfea56089f4edaa05d82584222acf3aa6f9
Shaun Saker <sakershaun@gmail.com>
2023-12-21 14:55:15 +1000

    feat(website): Added Open Graph metadata and automatic og image generation 🤓

--- packages/website/src/app/layout.tsx
+++ packages/website/src/app/layout.tsx
@@ -10,18 +10,28 @@ import { MainLayout } from '@/components/mainLayout/MainLayout'
 import { app, tailwindTheme } from '../../../config'
 
 export const metadata: Metadata = {
+  // NOTE: the icons and og images are automatically added to the head by Next.js
   title: {
     template: `%s - ${app.name}`,
-    default: `${app.name}`,
+    default: app.name,
   },
-  description: `${app.description}`,
+  description: app.description,
   themeColor: tailwindTheme.extend.colors.theme.brand.DEFAULT,
+  openGraph: {
+    title: app.name,
+    description: app.description,
+    url: process.env.NEXT_PUBLIC_URL,
+    siteName: app.name,
+    locale: 'en_US',
+    type: 'website',
+  },
+  metadataBase: new URL(process.env.NEXT_PUBLIC_URL),
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className="h-full scroll-smooth font-sans antialiased">
-      <body className="h-full bg-theme-background dark:bg-dark-theme-background">
+    <html lang="en" className="h-full">
+      <body className="bg-theme-background dark:bg-dark-theme-background">
         <AppProvider>
           <MainLayout>{children}</MainLayout>
 
